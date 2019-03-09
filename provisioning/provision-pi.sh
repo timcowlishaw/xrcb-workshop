8f7be7b0806e870b7eeded7d7863472d3014d56e
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-03-09 12:22:17 +0000

    make sure provisioning script updates everything

--- provisioning/provision-pi.sh
+++ provisioning/provision-pi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install -y vim xvfb sonic-pi-server build-essential
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 cargo install --git https://github.com/lpil/sonic-pi-tool/
+sudo rpi-update

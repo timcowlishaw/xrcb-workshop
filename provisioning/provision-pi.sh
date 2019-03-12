b0301c8057d4a3e4a3a10b037e5167ce5d493b6e
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-03-12 21:54:36 +0000

    provision python packages

--- provisioning/provision-pi.sh
+++ provisioning/provision-pi.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y vim xvfb sonic-pi-server build-essential
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 cargo install --git https://github.com/lpil/sonic-pi-tool/
+pip install -r ../sensors/requirements.txt
 sudo rpi-update

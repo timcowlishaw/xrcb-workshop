91a76b25974d9f6e1e4a46ac4bf36aa525b8497c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-03-13 21:31:14 +0000

    stub sensor patch in part 2

--- part-2.rb
+++ part-2.rb
@@ -1,21 +1,23 @@
-def part2
+def part2(sensor)
   notes = [39, 42, 46].map { |x| x + 12  * 4}
   play(notes.choose,
        release: 0.2,
        amp: rand * 0.5,
        divisor: 6,
        depth: 0.5,
-       cutoff: 75
+       cutoff: sensor
       )
   sleep rand * 0.2
 end
 
 
-use_synth :fm 
-with_fx :reverb, mix: 0.2 do
-  with_fx :echo do
-    loop do
-      part2
+live_loop :part2 do
+  use_real_time
+  use_synth :fm 
+  sensor = sync "/xrcb/ldr"
+  with_fx :reverb, mix: 0.2 do
+    with_fx :echo do
+      part2(sensor)
     end
   end
 end
